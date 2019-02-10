f1c68011c4c073194416bda31cc3be86a4f5b1b2
Luke Shannon <luke.shannon@gmail.com>
2019-02-10 10:45:08 -0500

    Added check to ensure Sudo is installed, removed updating the OS through the script. User should do this on their own before running it

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
-echo "Setting Up A Stock Stretch OS Installation"
-echo "Update apt-get"
-sudo apt-get update
-echo "Upgrade apt-get"
-sudo apt-get upgrade
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root: sudo ./setup.sh"
+  exit
+fi
+echo "Before running this script you need to ensure your OS is updated"
+echo "The set up of your OS gives you an option to do this"
+echo "Otherwise, from the terminal run:"
+echo "sudo apt-get update"
+echo "sudo apt-get upgrade"
+echo "reboot"
+echo "If your OS is updated type 'Y' to continue"
+read -s CONFIRMATION
+if [ "$CONFIRMATION" != "Y" ]; then
+  echo "Script is terminating. Update the OS and reboot, then re-run this"
+  exit 0;
+fi
+echo "Setting Up A Stock Stretch OS Installation To Stream video with Motion"
 echo "Enable the camera"
 sudo sh -c "sed -i '/start_x=0/c\start_x=1' /boot/config.txt"
 echo "Install Motion"
@@ -22,9 +34,3 @@ sudo sh -c "sed -i '/height 240/c\height 480' /etc/motion/motion.con"
 sudo sh -c "sed -i '/stream_quality 50/c\stream_quality 90' /etc/motion/motion.conf"
 sudo sh -c "sed -i '/stream_localhost on/c\stream_localhost off' /etc/motion/motion.conf"
 sudo sh -c "sed -i '/framerate 2/c\framerate 1000' /etc/motion/motion.conf"
-
-
-
-
-
-
